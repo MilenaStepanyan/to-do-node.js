4d0785ca44f35488bc937842529ee02ccaf4747f
imnotmilena <stepanyanmil7.ait@gmail.com>
2025-01-29 23:16:56 +0400

    errors for creating to do handled

--- controllers/to-doController.js
+++ controllers/to-doController.js
@@ -1,1 +1,13 @@
 import promisePool from "../pool/db.js";
+import { STATUS_CODES } from "../utils/status-codes.js";
+
+export const createToDo = (req,res)=>{
+    try{
+        const {title} = req.body
+        if(!title){
+            return res.status(STATUS_CODES.BAD_REQUEST).json({msg:"missing required fields"})
+        }
+    }catch(err){
+        return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({msg:"internal sever error"})
+    }
+}
