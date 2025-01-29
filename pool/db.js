3a65e6f10807eae9856fef5c81ce93deb67cc946
imnotmilena <stepanyanmil7.ait@gmail.com>
2025-01-29 21:36:25 +0400

    pool connection settled successfully

--- pool/db.js
+++ pool/db.js
@@ -2,4 +2,14 @@ import mysql from "mysql2";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const pool = mysql.createPool();
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  });
+  
+  const promisePool = pool.promise();
+  
+  export default promisePool;
