ad96169970aaee0ce3afe28f6eb845265a7d05fa
imnotmilena <stepanyanmil7.ait@gmail.com>
2025-01-29 23:22:30 +0400

    promisepool query made for creating request

--- controllers/to-doController.js
+++ controllers/to-doController.js
@@ -1,13 +1,22 @@
 import promisePool from "../pool/db.js";
 import { STATUS_CODES } from "../utils/status-codes.js";
 
-export const createToDo = (req,res)=>{
-    try{
-        const {title} = req.body
-        if(!title){
-            return res.status(STATUS_CODES.BAD_REQUEST).json({msg:"missing required fields"})
-        }
-    }catch(err){
-        return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({msg:"internal sever error"})
+export const createToDo = (req, res) => {
+  try {
+    const { title, description } = req.body;
+    if (!title) {
+      return res
+        .status(STATUS_CODES.BAD_REQUEST)
+        .json({ msg: "missing required fields" });
     }
-}
+    const [result] = promisePool.query(
+      `INSERT INTO tasks title,description VALUES (?,?)`,
+      [title, description || ""]
+    );
+
+  } catch (err) {
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "internal sever error" });
+  }
+};
