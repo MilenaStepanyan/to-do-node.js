d78769aa4a3917c4047d93b42318c1f28f934d51
imnotmilena <stepanyanmil7.ait@gmail.com>
2025-01-29 23:12:49 +0400

    status codes named

--- pool/db.js
+++ pool/db.js
@@ -3,13 +3,13 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-  });
-  
-  const promisePool = pool.promise();
-  
-  export default promisePool;
+  connectionLimit: 10,
+  host: "localhost",
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
+
+const promisePool = pool.promise();
+
+export default promisePool;
