93bb606780a4642229639f2904311450e460b8a8
imnotmilena <stepanyanmil7.ait@gmail.com>
2025-01-29 23:36:15 +0400

    added async await,and successfully completed the test

--- controllers/to-doController.js
+++ controllers/to-doController.js
@@ -1,7 +1,7 @@
 import promisePool from "../pool/db.js";
 import { STATUS_CODES } from "../utils/status-codes.js";
 
-export const createToDo = (req, res) => {
+export const createToDo = async(req, res) => {
   try {
     const { title, description } = req.body;
     if (!title) {
@@ -9,12 +9,13 @@ export const createToDo = (req, res) => {
         .status(STATUS_CODES.BAD_REQUEST)
         .json({ msg: "missing required fields" });
     }
-    const [result] = promisePool.query(
-      `INSERT INTO tasks title,description VALUES (?,?)`,
+    const [result] = await promisePool.query(
+      `INSERT INTO tasks (title,description )VALUES (?,?)`,
       [title, description || ""]
     );
     return res.status(STATUS_CODES.CREATED).json({msg:"Created successfully",id:result.id})
   } catch (err) {
+    console.log(err);
     return res
       .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
       .json({ msg: "internal sever error" });
