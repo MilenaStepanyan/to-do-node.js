6cbc7c4c561432ab89efa2808a0b9b90b37746f2
imnotmilena <stepanyanmil7.ait@gmail.com>
2025-01-29 23:45:51 +0400

    successfully passsed the test

--- routers/to-doRouter.js
+++ routers/to-doRouter.js
@@ -2,5 +2,5 @@ import express from "express";
 import { createToDo ,getToDos} from "../controllers/to-doController.js";
 const router = express.Router();
 router.post("/task", createToDo);
-router.post("/getTask", getToDos);
+router.get("/getTask", getToDos);
 export default router;
