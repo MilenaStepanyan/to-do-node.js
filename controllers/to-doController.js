097ede2085e97a0d683b3418a305414cab10fecb
imnotmilena <stepanyanmil7.ait@gmail.com>
2025-01-29 23:43:53 +0400

    get to do list controller handled

--- controllers/to-doController.js
+++ controllers/to-doController.js
@@ -1,7 +1,7 @@
 import promisePool from "../pool/db.js";
 import { STATUS_CODES } from "../utils/status-codes.js";
 
-export const createToDo = async(req, res) => {
+export const createToDo = async (req, res) => {
   try {
     const { title, description } = req.body;
     if (!title) {
@@ -13,11 +13,28 @@ export const createToDo = async(req, res) => {
       `INSERT INTO tasks (title,description )VALUES (?,?)`,
       [title, description || ""]
     );
-    return res.status(STATUS_CODES.CREATED).json({msg:"Created successfully",id:result.id})
+    return res
+      .status(STATUS_CODES.CREATED)
+      .json({ msg: "Created successfully", id: result.id });
   } catch (err) {
     console.log(err);
     return res
       .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
       .json({ msg: "internal sever error" });
+  }
+};
+
+export const getToDos = async (req, res) => {
+  try {
+    const [result] = await promisePool.query(`SELECT * FROM tasks`);
+    if (result.length === 0) {
+      return res.status(STATUS_CODES.NOT_FOUND).json({ msg: "No To do's" });
+    }
+    return res.status(STATUS_CODES.OK).json({ msg: "To-do list", result });
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+      .json({ msg: "internal server error" });
   }
 };
