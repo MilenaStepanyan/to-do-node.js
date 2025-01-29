24d8c98539752bd7b95fbf424cea06e0a04d5b00
imnotmilena <stepanyanmil7.ait@gmail.com>
2025-01-29 23:28:32 +0400

    used by the server,ready to go on test

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import taskRouter from "./routers/to-doRouter.js"
 import cors from "cors";
 const app = express();
 const PORT = 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/create",taskRouter)
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
